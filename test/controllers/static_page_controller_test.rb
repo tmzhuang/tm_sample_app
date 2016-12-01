522daf1e2268f6f55434236be170760848bb87dc
Tianming Zhuang <tianming.zhuang@gmail.com>
2016-12-01 11:20:10 -0500

    Static pages

---
+++ test/controllers/static_page_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class StaticPageControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_page_home_url
+    assert_response :success
+    assert_select 'title', 'Home | tm-sample-app'
+  end
+
+  test "should get help" do
+    get static_page_help_url
+    assert_response :success
+    assert_select 'title', 'Help | tm-sample-app'
+  end
+
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+    assert_select 'title', 'About | tm-sample-app'
+  end
+
+end
