075dbcc1b0e7bf8a09dc261702633726538c6505
Tianming Zhuang <tianming.zhuang@gmail.com>
2016-12-15 21:26:05 -0500

    Add secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Tianming Zhuang", email: "tianming.zhuang@gmail.com")
+    @user = User.new(name: "Tianming Zhuang", email: "tianming.zhuang@gmail.com",
+                    password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -33,5 +34,15 @@ class UserTest < ActiveSupport::TestCase
     user2 = @user.dup
     @user.save
     assert_not user2.valid?
+  end
+  
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should not be too short" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
